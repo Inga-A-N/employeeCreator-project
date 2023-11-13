5de58d6b7db5c396deb0f0bcb16c499de12e8beb
unknown <andreevamailbox@gmail.com>
2023-11-13 23:41:05 +1100

    wip form

--- employeeCreatorFrontend/src/components/CreateEmployeeForm/CreateEmployeeForm.tsx
+++ employeeCreatorFrontend/src/components/CreateEmployeeForm/CreateEmployeeForm.tsx
@@ -1,26 +1,51 @@
+import { useState } from "preact/hooks";
+
 function CreateEmployeeForm() {
-  const handleSubmit = (e) => {
+  const initialData = {
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    contractType: "Permanent",
+    startDate: "",
+    finishDate: "",
+    workBasis: "Full_time",
+    hoursPerWeek: 0,
+  };
+  const [newEmployeeData, setNewEmployeeData] = useState(initialData);
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    console.log(e.target.id);
+    const { name, value } = e.target;
+    setNewEmployeeData({ ...newEmployeeData, [name]: value });
+  };
+
+  const handleSubmit = (e: Event) => {
     e.preventDefault();
+    console.log(newEmployeeData, "data to send");
     console.log("submitted");
   };
 
   return (
     <>
-      <h1 className="text-xl font-bold">Create new employee</h1>
+      <h1 className="text-xl font-bold pb-7">Create new employee</h1>
 
       <form onSubmit={handleSubmit}>
         <div className="relative z-0 w-full mb-6 group">
           <input
             type="text"
-            name="first_name"
-            id="first_name"
+            name="firstName"
+            id="firstName"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-[#058078] peer"
             placeholder=" "
             required
+            onChange={handleChange}
           />
           <label
-            for="first_name"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            for="firstName"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             First name
           </label>
@@ -28,14 +53,15 @@ function CreateEmployeeForm() {
         <div className="relative z-0 w-full mb-6 group">
           <input
             type="text"
-            name="middle_name"
-            id="middle_name"
+            name="middleName"
+            id="middleName"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-[#058078] peer"
             placeholder=" "
+            onChange={handleChange}
           />
           <label
-            for="middle_name"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            for="middleName"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Middle name (optional)
           </label>
@@ -43,15 +69,16 @@ function CreateEmployeeForm() {
         <div className="relative z-0 w-full mb-6 group">
           <input
             type="text"
-            name="last_name"
-            id="last_name"
+            name="lastName"
+            id="lastName"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-[#058078] peer"
             placeholder=" "
             required
+            onChange={handleChange}
           />
           <label
-            for="last_name"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            for="lastName"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Last name
           </label>
@@ -64,10 +91,11 @@ function CreateEmployeeForm() {
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-[#058078] peer"
             placeholder=" "
             required
+            onChange={handleChange}
           />
           <label
             for="address"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Address
           </label>
@@ -82,10 +110,11 @@ function CreateEmployeeForm() {
               "
               placeholder=" "
               required
+              onChange={handleChange}
             />
             <label
               for="email"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Email (Ex. johnsmith@gmail.com)
             </label>
@@ -94,15 +123,17 @@ function CreateEmployeeForm() {
             <input
               type="tel"
               pattern="[0-9]+()"
-              name="phone"
-              id="phone"
+              minlength="8"
+              name="phoneNumber"
+              id="phoneNumber"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-[#058078] peer"
               placeholder=" "
               required
+              onChange={handleChange}
             />
             <label
-              for="phone"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              for="phoneNumber"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Phone number
             </label>
@@ -115,7 +146,7 @@ function CreateEmployeeForm() {
             <input
               id="type-option-1"
               type="radio"
-              name="types"
+              name="contractType"
               value="Permanent"
               className="w-4 h-4 text-[#04c4b2] border-gray-300 focus:ring-2 focus:ring-[#04c4b2] dark:focus:ring-[#058078] dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
               checked
@@ -132,9 +163,10 @@ function CreateEmployeeForm() {
             <input
               id="type-option-2"
               type="radio"
-              name="types"
+              name="contractType"
               value="Contract"
               className="w-4 h-4 text-[#04c4b2] border-gray-300 focus:ring-2 focus:ring-[#04c4b2] dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
+              onChange={handleChange}
             />
             <label
               for="type-option-2"
@@ -149,16 +181,17 @@ function CreateEmployeeForm() {
           <div className="relative z-0 w-full mb-6 group">
             <input
               type="date"
-              pattern="[0-9]-"
-              name="start_date"
-              id="start_date"
+              pattern="[0-9]"
+              name="startDate"
+              id="startDate"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-[#058078] peer"
               placeholder=" "
               required
+              onChange={handleChange}
             />
             <label
-              for="start_date"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              for="startDate"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Start date
             </label>
@@ -167,16 +200,17 @@ function CreateEmployeeForm() {
           <div className="relative z-0 w-full mb-6 group">
             <input
               type="date"
-              pattern="[0-9]-"
-              name="finish_date"
-              id="finish_date"
+              pattern="[0-9]"
+              name="finishDate"
+              id="finishDate"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-[#058078] peer"
               placeholder=" "
               required
+              onChange={handleChange}
             />
             <label
-              for="finish_date"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              for="finishDate"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#058078] peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Finish date
             </label>
@@ -190,7 +224,7 @@ function CreateEmployeeForm() {
             <input
               id="basis-option-1"
               type="radio"
-              name="basis"
+              name="workBasis"
               value="Full_time"
               className="w-4 h-4 text-[#04c4b2] border-gray-300 focus:ring-2 focus:ring-[#04c4b2] dark:focus:ring-[#058078] dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
               checked
@@ -207,7 +241,7 @@ function CreateEmployeeForm() {
             <input
               id="basis-option-2"
               type="radio"
-              name="basis"
+              name="workBasis"
               value="Part_time"
               className="w-4 h-4 text-[#04c4b2] border-gray-300 focus:ring-2 focus:ring-[#04c4b2] dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
             />
@@ -226,10 +260,13 @@ function CreateEmployeeForm() {
           </span>
           <input
             type="number"
-            pattern="[0-9]."
-            name="hours_per_week"
-            id="hours_per_week"
+            min="1"
+            step="0.1"
+            name="hoursPerWeek"
+            id="hoursPerWeek"
             className="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+            onChange={handleChange}
           />
         </div>
 
--- employeeCreatorFrontend/src/services/employees.ts
+++ employeeCreatorFrontend/src/services/employees.ts
@@ -13,3 +13,16 @@ export const deleteEmployeeById = async (id: Number) => {
     throw new Error("Could not remove employee");
   }
 };
+
+export const createNewEmployee = async (data) => {
+  const response = await fetch("http://localhost:8080/employees", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    throw new Error("Could not create new employee");
+  }
+};
